92736233fbd2a88504eabfcc16cb404c98d2145e
csy <caishuyang@efangtec.com>
2020-01-19 18:23:27 +0800

    解决iframe高度问题

--- src/main/resources/static/js/head/main.js
+++ src/main/resources/static/js/head/main.js
@@ -5,8 +5,20 @@ $(document).ready(function () {
       $(this).addClass('active');
    });
 });
-
+var id;
 function getUrl(url) {
     var main = $('#main');
-    main.html("<iframe border='none' style='border:0; padding:15px' scrolling='no' width='100%' height='2000' src='" + url + "'></iframe>");
+    id = new Date().getMilliseconds();
+    main.html("<iframe id='"+id+"' border='none' style='border:0; padding:15px' scrolling='no' width='100%' height='100%' onload='this.height=200' src='" + url + "'></iframe>");
 }
+function reinitIframe(){
+    var iframe = document.getElementById(id);
+    try{
+        var bHeight = iframe.contentWindow.document.body.scrollHeight;
+        var dHeight = iframe.contentWindow.document.documentElement.scrollHeight;
+        var height = Math.max(bHeight, dHeight);
+        iframe.height = height;
+        console.log(height);
+    }catch (ex){}
+}
+window.setInterval("reinitIframe(id)", 200);
