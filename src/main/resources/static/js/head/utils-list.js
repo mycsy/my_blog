24274e62e1d43c5932a02f009041cc0daae4813a
csy <caishuyang@efangtec.com>
2020-04-26 16:39:55 +0800

    调整

--- src/main/resources/static/js/head/utils-list.js
+++ src/main/resources/static/js/head/utils-list.js
@@ -11,4 +11,29 @@ $(function () {
 $('#delete_on_btn').click(function () {
     $("#delete_one").attr("action", config.url + "/front/pdf/delete/one").submit();
 });
-
+var right_data = new Vue({
+    el: '#right-box',
+    data: {
+        labelList: JSON.parse($.cookie("label_list")),
+        articleByDateVoList: JSON.parse($.cookie("date_list")),
+        topList: JSON.parse($.cookie("top_list"))
+    },
+    methods: {
+        go: function (article_id) {
+            window.location.href="article-detail.html?article_id="+article_id;
+        }
+    },
+    filters: {
+        formatDate(time) {
+            let date = new Date(time);
+            return timeStamp2String(date, 'yyyy-MM-dd');
+        },
+        labHandle(labList) {
+            var allLab = "";
+            labList.forEach(function (v) {
+                allLab += v['labelName'] + ',';
+            });
+            return allLab.substr(0, allLab.length - 1);
+        }
+    }
+});
