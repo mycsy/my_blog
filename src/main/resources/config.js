d32a86574b3ebd73af830bff280d53ac6e25cbc5
csy <caishuyang@efangtec.com>
2020-09-28 16:11:53 +0800

    鼠标移动特效

---
+++ src/main/resources/config.js
@@ -0,0 +1,35 @@
+var config = {
+    url: "http://180.76.242.43:8888"
+}
+function formDataToJson($form) {
+    var unindexed_array = $form.serializeArray();
+    var indexed_array = {};
+
+    $.map(unindexed_array, function (n, i) {
+        indexed_array[n['name']] = n['value'];
+    });
+    return JSON.stringify(indexed_array);
+}
+
+function queryString(name){
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) return unescape(r[2]);
+    return null;
+}
+
+function timeStamp2String(time, format){
+    var datetime = new Date();
+    datetime.setTime(time);
+    var year = datetime.getFullYear();
+    var month = datetime.getMonth() + 1 < 10 ? "0" + (datetime.getMonth() + 1) : datetime.getMonth() + 1;
+    var date = datetime.getDate() < 10 ? "0" + datetime.getDate() : datetime.getDate();
+    var hour = datetime.getHours()< 10 ? "0" + datetime.getHours() : datetime.getHours();
+    var minute = datetime.getMinutes()< 10 ? "0" + datetime.getMinutes() : datetime.getMinutes();
+    var second = datetime.getSeconds()< 10 ? "0" + datetime.getSeconds() : datetime.getSeconds();
+    if (format === 'yyyy-MM-dd') {
+        return year + "-" + month + "-" + date+" ";
+    } else {
+        return year + "-" + month + "-" + date + " " + hour + '-' + minute + '-' + second;
+    }
+}
